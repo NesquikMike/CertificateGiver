c49d24a20d7a5847a504148e48565290326dff7b
NesquikMike <mgomesvieira13@gmail.com>
2018-07-21 19:37:54 +0100

    makes spreadsheet of random names

--- certificateGiver.py
+++ certificateGiver.py
@@ -2,3 +2,15 @@ import pandas as pd
 import sys
 import random as rd
 
+if len(sys.argv) == 3:
+    recentWeeksToIgnore = int(sys.argv[2])
+    certificatesToGive = int(sys.argv[1])
+elif len(sys.argv) == 2:
+    recentWeeksToIgnore = 3
+    certificatesToGive = int(sys.argv[1])
+else:
+    recentWeeksToIgnore = 3
+    certificatesToGive = 5
+
+db = pd.read_csv("certificatesSpreadsheetTest.csv")
+
